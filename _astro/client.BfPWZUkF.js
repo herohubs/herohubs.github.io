b386972e00e04d3937d04653f7024b37ac9f0757
ranieri-vannozzi <ranieri-vannozzi@users.noreply.github.com>
2025-12-24 12:02:20 +0000

    deploy: 62a69880f2996693eb56e1b2c37efd5c63db7df9

--- _astro/client.BfPWZUkF.js
+++ _astro/client.BfPWZUkF.js
@@ -1,4 +1,4 @@
-import{r as s1,a as ia}from"./index.BgwVLlj0.js";var ai={exports:{}},de={},ui={exports:{}},ei={};/**
+import{a as s1,r as ia}from"./index.Cd_vQiNd.js";var ai={exports:{}},de={},ui={exports:{}},ei={};/**
  * @license React
  * scheduler.production.js
  *
