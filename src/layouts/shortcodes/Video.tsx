7046f56e1a9946079e1e5b161884d6d586a8488b
Ranieri Vannozzi <rvannozzi@herohubs.it>
2026-05-19 17:52:44 +0200

    Add article: Why is ignoring package-lock architectural suicide? (draft: true)

--- src/layouts/shortcodes/Video.tsx
+++ src/layouts/shortcodes/Video.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
-function Video({
-  title,
-  src,
-  ...rest
-}: {
-  title: string;
-  src: string;
-  [key: string]: any;
-}) {
+function Video({ title, src, ...rest }: { title: string; src: string; [key: string]: any }) {
   return (
     <div className="w-full overflow-hidden rounded-lg">
       <video className="w-full" controls {...rest}>
